dc11547d8b2c2e6d7e0fa444dbb01beb0b69d4be
unknown <matheusbortolini@hotmail.com>
2019-11-16 16:30:35 -0300

    send test message

---
+++ sendMatches.js
@@ -0,0 +1,54 @@
+const request = require('request')
+
+const matchArray = require('./matchArrayTeste.json')
+
+async function run(matchArray) {
+    await processMatchArray(matchArray)
+}
+
+async function processMatchArray(matchArray) {
+    for (let match of matchArray) {
+        await processMatch(match)
+    }
+}
+
+async function processMatch(match) {
+    let message = `[TESTE]
+Olá, ${match.fromName}!
+    
+Seu amigo(a) secreto(a) é *${match.toName}*`
+
+    await sendMessage(match.fromPhone, message).then((response) => {
+        console.log("Message sent", match)
+    }, (error) => {
+        console.log("Error sending message", match, error)
+    })
+}
+
+function sendMessage(phone, text) {    
+    let bodyRequest = {
+        phone: phone,
+        body: text
+    }
+
+    /*let bodyRequest = {
+        phone: "554196646823",
+        body: text
+    }*/
+    
+    let options = {
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(bodyRequest),
+        method: "POST",
+        url: `https://eu16.chat-api.com/instance49220/sendMessage?token=hgfhib4vxhnntp0b`
+    }
+    
+    return new Promise((resolve, reject) => {
+        request(options, (error, response, body) => {
+        if (error) reject(error)
+            resolve(body)
+        })
+    })
+}
+
+run(matchArray)
